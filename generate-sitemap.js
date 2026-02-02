992be7fafcbee018e61aef7d33fbecbc4eea15ac
Ahmad Rizal <ahmaddrizalul@gmail.com>
2026-02-02 15:56:15 +0700

    Update package-lock.json and package.json with new dependencies

---
+++ generate-sitemap.js
@@ -0,0 +1,23 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+import fs from "fs";
+
+const links = [
+  { url: "/", changefreq: "daily", priority: 1.0 },
+  { url: "/about", changefreq: "monthly", priority: 0.8 },
+  { url: "/experience", changefreq: "monthly", priority: 0.8 },
+  { url: "/certification", changefreq: "monthly", priority: 0.8 },
+  { url: "/project", changefreq: "monthly", priority: 0.8 },
+  { url: "/contact", changefreq: "monthly", priority: 0.8 },
+];
+
+(async () => {
+  const sitemap = new SitemapStream({
+    hostname: "https://zal-portfolio.vercel.app",
+  });
+
+  links.forEach((link) => sitemap.write(link));
+  sitemap.end();
+
+  const data = await streamToPromise(sitemap);
+  fs.writeFileSync("./public/sitemap.xml", data.toString());
+})();
